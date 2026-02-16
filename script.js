03df7a7fb34c3f7892caae708d079b19d7be4cf9
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-02-16 17:53:32 -0500

    shrunk enemies on hit and created projectile explosion on hit

--- script.js
+++ script.js
@@ -63,6 +63,35 @@ class Enemy {
         this.y = this.y + this.velocity.y
     }
 }
+const friction = 0.99
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+    draw() {
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.restore()
+    }
+
+    update() {
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
@@ -70,6 +99,7 @@ const y = canvas.height / 2
 const player = new Player(x, y, 10, 'white')
 const projectiles = []
 const enemies = []
+const particles = []
 
 function spawnEnemies() {
     setInterval(() => {
@@ -116,6 +146,13 @@ function animate() {
     c.fillStyle = 'rgba(0, 0, 0, 0.1)'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else {
+            particle.update()
+        }
+    });
     projectiles.forEach((projectile, index) => {
         projectile.update()
 
@@ -144,12 +181,33 @@ function animate() {
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
-            // objects touch
+            // when projectiles touch enemy
             if (dist -enemy.radius - projectile.radius < 1) {
-                setTimeout(() => {
-                enemies.splice(index, 1)
-                projectiles.splice(projectileIndex, 1)
-                }, 0)
+
+                // create explosions
+                for (let i = 0; i < enemy.radius * 2; i++) {
+                    particles.push(
+                        new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, 
+                            {x: (Math.random() - 0.5) * (Math.random() * 6), y: (Math.random() - 0.5) * (Math.random() * 6),
+
+                        })
+                    )
+                }
+
+                if (enemy.radius - 10 > 5) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                        setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+
+                } else {
+                        setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
             }
         });
     })
